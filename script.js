f029cd380f7dc24934ba76bc7a80c12e73307111
whkessler <82409360+whkessler@users.noreply.github.com>
2021-04-29 13:31:32 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var jane = document.querySelector('#tagname') 
+var remove = document.querySelector('#todd')
+var del = document.querySelector("#phil")
+
+function profile(){
+    jane.innerText = "Alayne T";
+}
+function reqs(){
+    remove.innerText = "";
+}
+function reqs1(){
+    del.innerText = "";
+}
